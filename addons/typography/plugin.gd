ef8fb9c3ea93f37d25ed0a24029b53ef44ffa0c9
imjp94 <imjp_94@hotmail.com>
2020-09-05 01:58:28 +0800

    Let OverlayTextEdit handle its on event

--- addons/typography/plugin.gd
+++ addons/typography/plugin.gd
@@ -5,9 +5,8 @@ const OverlayTextEdit = preload("scenes/OverlayTextEdit.tscn")
 
 var menu
 var overlay_text_edit
-var editor_inspector = get_editor_interface().get_inspector()
 
-var _object_orig_text = ""
+var editor_inspector = get_editor_interface().get_inspector()
 
 
 func _enter_tree():
@@ -15,13 +14,13 @@ func _enter_tree():
 	menu.undo_redo = get_undo_redo()
 	menu.connect("property_edited", self, "_on_TypographyMenu_property_edited")
 	overlay_text_edit = OverlayTextEdit.instance()
-	overlay_text_edit.set_as_toplevel(true)
-	overlay_text_edit.connect("focus_exited", self, "_on_OverlayTextEdit_focused_exited")
-	overlay_text_edit.connect("text_changed", self, "_on_OverlayTextEdit_text_changed")
+	overlay_text_edit.undo_redo = get_undo_redo()
+	overlay_text_edit.connect("property_edited", self, "_on_OverlayTextEdit_property_edited")
 
 	editor_inspector.connect("property_selected", self, "_on_property_selected")
 
 	add_control_to_container(EditorPlugin.CONTAINER_CANVAS_EDITOR_BOTTOM, menu)
+	add_control_to_container(EditorPlugin.CONTAINER_CANVAS_EDITOR_BOTTOM, overlay_text_edit)
 
 func _exit_tree():
 	if menu:
@@ -38,46 +37,21 @@ func handles(object):
 
 func edit(object):
 	menu.focused_object = object
+	overlay_text_edit.focused_object = object
 
 func forward_canvas_gui_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT:
 			if event.doubleclick:
 				if menu.focused_object:
-					_on_double_click_object(menu.focused_object)
+					overlay_text_edit.popup()
 				return true
-
 	return false
 
 func make_visible(visible):
 	if menu:
 		menu.visible = visible
-
-func _on_double_click_object(object):
-	if "text" in object:
-		if overlay_text_edit.get_parent():
-			overlay_text_edit.get_parent().remove_child(overlay_text_edit)
-		menu.add_child(overlay_text_edit) 
-		overlay_text_edit.rect_global_position = get_viewport().get_mouse_position()
-		overlay_text_edit.rect_size = object.rect_size
-		overlay_text_edit.text = object.text
-		overlay_text_edit.grab_focus()
-
-		_object_orig_text = object.text
-
-func _on_OverlayTextEdit_text_changed():
-	if menu.focused_object:
-		# TODO: Option to set bbcode_text if is RichTextLabel
-		menu.focused_object.set("text", overlay_text_edit.text)
-
-func _on_OverlayTextEdit_focused_exited():
-	if overlay_text_edit.get_menu().visible: # Support right-click context menu
-		return
-
-	if overlay_text_edit.get_parent():
-		overlay_text_edit.get_parent().remove_child(overlay_text_edit)
-		# TODO: More efficient way to handle undo/redo of text, right now, whole chunks of string is cached everytime
-		change_text(menu.focused_object, overlay_text_edit.text)
+	# overlay_text_edit only visible on double click
 
 func _on_property_selected(property):
 	menu.focused_property = property
@@ -86,15 +60,5 @@ func _on_property_selected(property):
 func _on_TypographyMenu_property_edited(property):
 	editor_inspector.refresh()
 
-func change_text(object, to):
-	var from = _object_orig_text
-	var undo_redo = get_undo_redo()
-	undo_redo.create_action("Change Text")
-	undo_redo.add_do_method(self, "set_text", object, to)
-	undo_redo.add_undo_method(self, "set_text", object, from)
-	undo_redo.commit_action()
-	_object_orig_text = ""
-
-func set_text(object, text):
-	object.set("text", text)
+func _on_OverlayTextEdit_property_edited(property):
 	editor_inspector.refresh()
---
+++ addons/typography/scenes/OverlayTextEdit.gd
@@ -0,0 +1,54 @@
+tool
+extends TextEdit
+
+signal property_edited(property)
+
+var focused_object
+var undo_redo
+
+var _object_orig_text = ""
+
+func _ready():
+	set_as_toplevel(true)
+	connect("focus_exited", self, "_on_focused_exited")
+	connect("text_changed", self, "_on_text_changed")
+	hide()
+
+func _on_text_changed():
+	if focused_object:
+		# TODO: Option to set bbcode_text if is RichTextLabel
+		focused_object.set("text", text)
+
+func _on_focused_exited():
+	if get_menu().visible: # Support right-click context menu
+		return
+
+	hide()
+	# TODO: More efficient way to handle undo/redo of text, right now, whole chunks of string is cached everytime
+	change_text(focused_object, text)
+
+func popup():
+	if not focused_object:
+		return
+	if not ("text" in focused_object):
+		return
+
+	show()
+	rect_global_position = get_viewport().get_mouse_position()
+	rect_size = focused_object.rect_size
+	text = focused_object.text
+	grab_focus()
+
+	_object_orig_text = focused_object.text
+
+func change_text(object, to):
+	var from = _object_orig_text
+	undo_redo.create_action("Change Text")
+	undo_redo.add_do_method(self, "set_object_text", object, to)
+	undo_redo.add_undo_method(self, "set_object_text", object, from)
+	undo_redo.commit_action()
+	_object_orig_text = ""
+
+func set_object_text(object, text):
+	object.set("text", text)
+	emit_signal("property_edited", "text")
